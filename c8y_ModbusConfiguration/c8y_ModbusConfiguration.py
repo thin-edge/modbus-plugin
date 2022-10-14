c04f91538a3c72c113d1b52e276d2f70d91c18b6
mbay <murat.bayram@softwareag.com>
2022-10-14 14:33:21 +0200

    initial commit

---
+++ c8y_ModbusConfiguration/c8y_ModbusConfiguration.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# coding=utf-8
+import sys
+from paho.mqtt import client as mqtt_client
+import os
+
+broker = 'localhost'
+port = 1883
+client_id = 'c8y_ModbusConfiguration-operation-client'
+
+
+command = sys.argv[1].split(',')[2]
+client = mqtt_client.Client(client_id)
+client.connect(broker, port)
+
+
+client.publish('c8y/s/us','501,c8y_ModbusConfiguration')
+#Config file save logic
+client.publish('c8y/s/us',f'503,c8y_ModbusConfiguration,""')
