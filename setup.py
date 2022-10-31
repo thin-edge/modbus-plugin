cda1dec82b5951f157ae75d06f5279dd78e379fc
Mario Heidenreich <mario.heidenreich@softwareag.com>
2022-10-31 13:42:56 +0100

    add systemd service, log folder and link binary

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import setup, find_packages
 setup(
     name='te-modbus-reader',
     version='0.1',
-    description='Modbus plugin for ',
-    url='http://github.com/trstringer/python3-random-quote',
+    description='Modbus plugin for thin-edge',
+    url='https://github.com/thin-edge/modbus-plugin',
     author='Software AG ♥ thin-edge.io',
-    author_email='github@trstringer.com',
+    author_email='info@thin-edge.io',
     license='MIT',
     install_requires=[
         'paho-mqtt~=1.6.1',
@@ -19,6 +19,7 @@ setup(
         'tomli~=2.0.1',
         'pyserial-asyncio'],
     packages=find_packages(),
+    data_files=[('config/devices.toml', ['config/devices.toml']), ('config/modbus.toml', ['config/modbus.toml'])],
     entry_points=dict(
         console_scripts=['te-modbus-reader=modbus_reader.reader:main']
     )
