f4f767faeb149ae410accb1f000e6bdabaab5a15
wilbers.lucas <wilbers@a-storm.com>
2026-03-19 13:05:53 +0100

    Bugfix, assign topic for all device names

--- tedge_modbus/reader/reader.py
+++ tedge_modbus/reader/reader.py
@@ -622,11 +622,11 @@ class ModbusPoll:
     def register_child_devices(self, devices):
         """Register the child devices with thin-edge.io"""
         for device in devices:
+            topic = f"te/device/{device['name']}//"
             if device["name"] != "main":
                 self.logger.debug(
                     "Child device registration for device %s", device["name"]
                 )
-                topic = f"te/device/{device['name']}//"
                 payload = {
                     "@type": "child-device",
                     "name": device["name"],
@@ -635,6 +635,8 @@ class ModbusPoll:
                 self.send_tedge_message(
                     MappedMessage(json.dumps(payload), topic), retain=True, qos=1
                 )
+
+            self.logger.debug("Set command registration for device %s", device["name"])
             cmd_payload = "{}"
             for cmd in ["modbus_SetRegister", "modbus_SetCoil"]:
                 cmd_topic = topic + f"/cmd/{cmd}"
